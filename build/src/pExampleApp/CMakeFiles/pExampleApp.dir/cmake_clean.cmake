bf889b5df97c8825d89b425820b0a0dbc722fdfd
Olaf Perzanowski <olafperzanowski@gmail.com>
2025-04-12 10:58:11 +0000

    First commit

---
+++ build/src/pExampleApp/CMakeFiles/pExampleApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/vboxuser/Desktop/moos-ivp-extend/bin/pExampleApp"
+  "/home/vboxuser/Desktop/moos-ivp-extend/bin/pExampleApp.pdb"
+  "CMakeFiles/pExampleApp.dir/main.cpp.o"
+  "CMakeFiles/pExampleApp.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pExampleApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
